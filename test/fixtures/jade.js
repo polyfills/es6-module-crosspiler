9d924da8d4f725b85320c2794d7ca332cea584b5
Jonathan Ong <jonathanrichardong@gmail.com>
2014-08-07 00:35:55 -0700

    add a normalize-like jade test

---
+++ test/fixtures/jade.js
@@ -0,0 +1,9 @@
+import jade from "https://nlz.io/github/visionmedia/jade/1.5.0/lib/runtime.js";
+
+export default function template(locals) {
+var buf = [];
+var jade_mixins = {};
+var jade_interp;
+
+buf.push("<!DOCTYPE html><html><head></head><body></body></html>");;return buf.join("");
+}
--- test/transform.js
+++ test/transform.js
@@ -30,4 +30,16 @@ describe('.transform()', function () {
     vm.runInThisContext('if (typeof exports.a !== "function") throw new Error()')
     vm.runInThisContext('if (typeof exports.b !== "function") throw new Error()')
   })
+
+  it('jade', function () {
+    var ast = read('jade')
+    var m = Module(ast)
+    m.transform()
+    var result = recast.print(m.ast)
+    var context = vm.createContext()
+    vm.runInThisContext(vmExports, context)
+    vm.runInThisContext(vmRequire, context)
+    vm.runInThisContext(result.code, context)
+    vm.runInThisContext('if (typeof exports.default !== "function") throw new Error()')
+  })
 })
